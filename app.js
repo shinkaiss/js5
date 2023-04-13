bcf40d346f22366ffe78581677cb5fd86a7229d7
shinkaiss <saikalormonovass@gmail.com>
2023-04-13 18:42:51 +0600

    first commit

--- app.js
+++ app.js
@@ -3,8 +3,30 @@ let obj = arrayToObject(arr);
 function arrayToObject(arr) {
   let obj = {};
   for (let i = 0; i < arr.length; i += 2) {
-    obj[arr[i]] = arr[i+1];
+    obj[arr[i]] = arr[i + 1];
   }
   return obj;
 }
-console.log(obj) 
+console.log(obj);
+
+
+// dop dz
+function calculateAverage() {
+  let sum = 0;
+  let count = 0;
+
+  for (let i = 0; i < arguments.length; i++) {
+    if (typeof arguments[i] === "number") {
+      sum += arguments[i];
+      count++;
+    }
+  }
+
+  if (count === 0) {
+    console.log("No numbers were provided");
+    return;
+  }
+
+  const average = sum / count;
+  console.log("The average of the provided numbers is: "` ${average}`);
+}
