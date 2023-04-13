302b4eabf89b8967cf811df0133815499e9049e6
shinkaiss <saikalormonovass@gmail.com>
2023-04-13 18:53:17 +0600

    first commit

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-let arr = ["name", "John", "lastname", "Black", "age", "23"];
-let obj = arrayToObject(arr);
-function arrayToObject(arr) {
-  let obj = {};
-  for (let i = 0; i < arr.length; i += 2) {
-    obj[arr[i]] = arr[i + 1];
-  }
-  return obj;
-}
-console.log(obj);
-
-
 // dop dz
 function calculateAverage() {
   let sum = 0;
